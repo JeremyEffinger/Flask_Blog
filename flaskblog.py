2704bf5d7690b3acae1e2d72e162b4bf0d942147
Jeremy Effinger <Jeremy.Effinger@gmail.com>
2020-05-23 14:37:10 -0700

    Login and Registration pages made

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "11df6318f394029e2669979c288eadb9"
 posts = [
     {
         "author": "Jeremy Effinger",
@@ -27,6 +29,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("Register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful, please check username and password.", "danger")
+    return render_template("Login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
