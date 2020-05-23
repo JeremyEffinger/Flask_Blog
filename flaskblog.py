25420888ded32f95f2a1a056cec7e81e470fcd91
Jeremy Effinger <Jeremy.Effinger@gmail.com>
2020-05-22 19:29:05 -0700

    basic styling added

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Jeremy Effinger",
+        "title": "First Post",
+        "content": "First Post Content",
+        "date_posted": "May 22, 2020",
+    },
+    {
+        "author": "Jamie Effinger",
+        "title": "Second Post",
+        "content": "Second Post Content",
+        "date_posted": "May 23, 2020",
+    },
+]
+
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
